8d93cf8d3efc8e25b5684686bb3cb40080254444
Meoyoug <eodyd426@gmail.com>
2024-03-08 23:38:52 +0900

    오류좀그만

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,4 +10,4 @@ daphne -b 0.0.0.0 -p 8000 app.route:application
 
 # uWSGI: Nginx 로부터 데이터를 받아오면 장고랑 소통하는 역할
 # 파이썬 워커는 4 개 쓸거고 쓰레드를 사용가능하게 하겠다.라는 의미.
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+uwsgi --socket :8000 --workers 4 --master --enable-threads --module app.wsgi
