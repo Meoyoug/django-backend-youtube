d36f63380cf595f8f010974de73793fc72e4274b
Meoyoug <eodyd426@gmail.com>
2024-03-09 06:03:14 +0900

    오류좀그만

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "'django' service is up - executing command"
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/nginx.conf > /etc/nginx/conf.d/nginx.conf
 nginx -g 'daemon off;'
