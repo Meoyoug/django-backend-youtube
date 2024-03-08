fa8af09a4816605c55e91e6b75baeec23910412b
Meoyoug <eodyd426@gmail.com>
2024-03-09 04:01:02 +0900

    오류좀그만

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-
-# 'app' 서비스가 시작되기를 기다립니다.
-until nc -z $APP_HOST $APP_PORT; do
-    echo "Waiting for the 'app' service..."
-    sleep 1
-done
+set -e
 
 echo "'app' service is up - executing command"
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
