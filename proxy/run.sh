230bb59acb6b530e5afe3a7b13611b0788842afa
Meoyoug <eodyd426@gmail.com>
2024-03-11 17:01:03 +0900

    daphne

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "'django' service is up - executing command"
-envsubst < /etc/nginx/nginx.conf > /etc/nginx/conf.d/nginx.conf
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
